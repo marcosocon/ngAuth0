b24d88d60169ab734f4e9106d6800ab5bf8cc1bd
marcosocon <oconmarcos97@gmail.com>
2016-10-04 23:57:00 -0300

    added router

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { routing, appRoutingProviders }  from './app.routing';
 
 import { AppComponent }  from './app.component';
 import { ProfileComponent }  from './components/profile/profile.component';
@@ -7,8 +8,9 @@ import { HomeComponent }  from  './components/home/home.component';
 
 
 @NgModule({
-  imports: [ BrowserModule ],
+  imports: [ BrowserModule, routing ],
   declarations: [ AppComponent, ProfileComponent, HomeComponent ],
+  providers: [ appRoutingProviders ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
